6356b7d6a757d0b1d3edcf1dd68d28f0fd85c25b
Monu Dixit <monudixit0007@gmail.com>
2024-10-11 21:52:46 +0530

    issue 4

--- index.js
+++ index.js
@@ -27,16 +27,16 @@ const PORT = process.env.PORT || 3002;
 //   origin: `${process.env.CLIENT_URL}`  // Replace with your frontend URL
 // }));
 
-// app.use(
-//     cors(
-//       {
-//       origin: `${process.env.CLIENT_URL}` || "http://localhost:3000" ,
-//       // origin:"http://localhost:3000",
-//       methods: ["GET", "POST"],
-//       credentials: true,
-//     }
-//   )
-//   );
+app.use(
+    cors(
+      {
+      origin: `${process.env.CLIENT_URL}` || "http://localhost:3000"  ,
+      // origin:"http://localhost:3000",
+      methods: ["GET", "POST"],
+      credentials: true,
+    }
+  )
+  );
 
   // Middleware to parse JSON and URL-encoded bodies
   app.use(express.json());
